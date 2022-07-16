a511f923a0d94042951e92b8f372d859a918de16
Yathin S Rao <63180414+yat2k@users.noreply.github.com>
2022-07-16 11:45:35 +0530

    Update firebase.js

--- src/components/Pages/firebase.js
+++ src/components/Pages/firebase.js
@@ -3,13 +3,7 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCoEgMv9At61eEkhwJzDwCqGsABo31yG4o",
-    authDomain: "alaap-a796c.firebaseapp.com",
-    projectId: "alaap-a796c",
-    storageBucket: "alaap-a796c.appspot.com",
-    messagingSenderId: "426055283375",
-    appId: "1:426055283375:web:87088c46578d199b2f5a5a",
-    measurementId: "G-1WBQ5MY670"
+    //Cant be disclosed.
   };
 
 // Initialize Firebase
